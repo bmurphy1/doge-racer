92dc9011ae0942e1d4b7b34d9f23463a6a3bbfe2
Brian Murphy <bmurphy1@mac.com>
2014-02-04 17:52:54 -0600

    Finalize JavaScript object oriented refactor

--- public/js/js_racer.js
+++ public/js/js_racer.js
@@ -1,122 +1,84 @@
+function player(name) {
+  this.name = name;
+  position = 1;
+}
 
-// var time = 0;
-// var timer = function (action) {
-//       var d = Date.now();
-//       if (time < 1 || action === 'start') {
-//           time = d;
-//           return 0;
-//       } else if (action === 'stop') {
-//           var t = d - time;
-//           time = 0;
-//           return t;
-//       } else {
-//           return d - time;
-//       }
-//   };
+function game(id) {
+  this.id = id;
+}
 
+game.prototype.start = function() {
+  console.log("MUCH STARTING! MANY GO! WOW!")
+  this.duration = Date.now();
+}
 
-// var update_player_position = function (player) {
-//   $("#" + player + "_strip .active").next().addClass('active');
-//   $("#" + player + "_strip .active").prev().removeClass('active');
-// }
+game.prototype.finish = function() {
+  console.log("GAME OVER! MUCH WIN. MANY HAPPY. WOW.")
+  this.duration = Date.now() - this.duration;
+}
 
-// $(document).ready(function() {
-//   timer('start');
-// //   if($("#player1_strip #second-box").css('active')) {
-// //       $('#game-start').css('display','none');
-// //   }
+function countDown(count) {
+  console.log('countdown has fired')
+  var check = function() {
+    console.log("check function");
+    console.log(count);
+    if (count === -1) {
+      $("#giant-doge").html("");
+      $("#go").html("");
+      fireEverything();
+    } else if(count === 0){
+      $("#countdown").html("");
+      $("#go").html("GO!");
+      setTimeout(check,1000);
+      count --;
+    } else {
+      $("#countdown").html(count);
+      setTimeout(check,1000);
+      count --;
+    }
+  }
+  check();
+}
 
-//   $(document).keyup(function(event) {
-//     if(event.keyCode == 81){update_player_position("player1");}
-//     if(event.keyCode == 80){update_player_position("player2");}
-//     // Detect which key was pressed and call the appropriate function
-//     // Google "jquery keyup what key was pressed" if you don't know how
-//     if($("#player1_strip #second-box").hasClass("active")) {
-//       $('#game-start').css('display','none');
-//     }
-//     if($("#player1_strip .last-box").hasClass("active")) {
-//       $('#game-end').css('display','inline');
-//       var timer_result = timer('stop');
-//       console.log("Time was " + timer_result);
-//       $.post("/end_game",{player:1, duration:timer_result}, function(results_page) {
-//         console.log("This part is firing");
-//         console.log(results_page);
-//         window.location.href = results_page
-//       });
-//       $("td").removeClass("last-box");
-//     } else if($("#player2_strip .last-box").hasClass("active")) {
-//       $('#game-end').css('display','inline');
-//       var timer_result = timer('stop');
-//       console.log("Time was " + timer_result);
-//       $.post("/end_game",{player:2, duration:timer_result}, function(results_page) {
-//         console.log("This part is firing");
-//         console.log(results_page);
-//         window.location.href = results_page
-//       });
-//       $("td").removeClass("last-box");
-//     }
-//   });
-// });
+$(document).ready(function() {
+  console.log("one line before countdown");
+  countDown(3);
+  console.log("one line after countdown");
+});
 
+fireEverything = function() {
+  currentGame = new game(1);
 
-// ================================================ //
-
-// $(document).ready(function() {
-//   var player1 = new Player("jim");
-//   var player2 = new Player("anne");
-
-//   var game = new Game(player1, player2);
-
-//   $(document).on('keyup', function(event) {
-//     game.onKeyUp(event.which);
-//   });
-// });
-
-
-var time = 0;
-var timer = function (action) {
-      var d = Date.now();
-      if (time < 1 || action === 'start') {
-          time = d;
-          return 0;
-      } else if (action === 'stop') {
-          var t = d - time;
-          time = 0;
-          return t;
-      } else {
-          return d - time;
+    currentGame.start();
+    $(document).keyup(function(event) {
+      if(event.keyCode == 81){update_player_position("player1");}
+      if(event.keyCode == 80){update_player_position("player2");}
+      // Detect which key was pressed and call the appropriate function
+      // Google "jquery keyup what key was pressed" if you don't know how
+      if($("#player1_strip #second-box").hasClass("active")) {
+        $('#game-start').css('display','none');
       }
-  };
-
+      if($("#player1_strip .last-box").hasClass("active")) {
+        // Maybe send duration to endGame function alongside winning player...
+        currentGame.finish();
+        console.log(currentGame.duration);
+        endGame(1, currentGame.duration);
+      } else if($("#player2_strip .last-box").hasClass("active")) {
+        currentGame.finish();
+        console.log(currentGame.duration);
+        endGame(1, currentGame.duration);
+      }
+    });
+}
 
 var update_player_position = function (player) {
   $("#" + player + "_strip .active").next().addClass('active');
   $("#" + player + "_strip .active").prev().removeClass('active');
 }
 
-$(document).ready(function() {
-  timer('start');
-
-  $(document).keyup(function(event) {
-    if(event.keyCode == 81){update_player_position("player1");}
-    if(event.keyCode == 80){update_player_position("player2");}
-    // Detect which key was pressed and call the appropriate function
-    // Google "jquery keyup what key was pressed" if you don't know how
-    if($("#player1_strip #second-box").hasClass("active")) {
-      $('#game-start').css('display','none');
-    }
-    if($("#player1_strip .last-box").hasClass("active")) {
-      endGame(1);
-    } else if($("#player2_strip .last-box").hasClass("active")) {
-      endGame(2);
-    }
-  });
-});
-
-var endGame = function(player) {
+var endGame = function(player, duration) {
   $('#game-end').css('display','inline');
-  var timer_result = timer('stop');
-  $.post("/end_game",{player:player, duration:timer_result}, function(results_page) {
+  $.post("/end_game",{player:player, duration:duration}, function(results_page) {
     window.location.href = results_page;
   });
   $("td").removeClass("last-box");
